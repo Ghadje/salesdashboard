00f36aad5f3bb0e0e47a341522fbc444c3138873
oussama.ghedjati <ouss.ghedje@gmail.com>
2024-11-28 08:53:15 +0100

    Clean and refactor the code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -23,9 +23,8 @@ function Home() {
   }, [isAuthenticated, loading, router]);
 
   if (loading) {
-    return <p>Loading...</p>; // Display a spinner or loader while checking auth state
+    return <p>Loading...</p>;
   }
-
 
   if(!loading && !isAuthenticated){
     return (
@@ -36,26 +35,26 @@ function Home() {
             <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
                 <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
                 <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-                    <strong>Bienvenue.</strong> Ceci est le tableau de bord {' '}
-                    <a href="https://iris.dz/" className="text-orange-500">
-                    Iris
-                    </a>
-                    , pour les comercials.
+                  <strong>Bienvenue.</strong> Ceci est le tableau de bord {' '}
+                  <a href="https://iris.dz/" className="text-orange-500">
+                  Iris
+                  </a>
+                  , pour les comercials.
                 </p>
                 <Button
-                    onClick={isAuthenticated ? ()=>router.push('/dashboard/home') : ()=> router.push('/login')}
-                    className="flex items-center gap-5 self-start rounded-lg bg-orange-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-orange-400 md:text-base"
+                  onClick={()=> router.push('/login')}
+                  className="flex items-center gap-5 self-start rounded-lg bg-orange-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-orange-400 md:text-base"
                 >
                 Se connecter <ArrowRightIcon className="w-5 md:w-6" />
                 </Button>
                 </div>
                 <div className="flex items-center justify-center p-6 md:w-4/5 md:px-28 md:py-0">
                     <Image
-                        src="/hero-desktop1.png"
-                        width={1000}
-                        height={760}
-                        className="hidden md:block"
-                        alt="Screenshots of the dashboard project showing desktop version"
+                      src="/hero-desktop1.png"
+                      width={1000}
+                      height={760}
+                      className="hidden md:block"
+                      alt="Screenshots of the dashboard project showing desktop version"
                     />
                 </div>
             </div>
